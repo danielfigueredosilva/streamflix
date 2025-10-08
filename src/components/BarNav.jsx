810a0d9a0c78861bbf808b4283ece089207d5155
Tulio Barroso <154523193+TulioBarroso@users.noreply.github.com>
2025-10-08 10:55:05 -0300

    Icone, estilização inicial da navbar
    Botei um icone provisorio e comecei a estlizei a navbar

--- src/components/BarNav.jsx
+++ src/components/BarNav.jsx
@@ -1,29 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./BarNav.css";
+import Logo from '../assets/play-svgrepo-com.svg';
 
 const BarNav = () => {
     return(
-        <nav className="nav-bar">
-            <Link to ='/' className="navbar-logo">
-                LOGO
-            </Link>
-            <div className="navbar-links">
-                <Link to="/" className="navbar-link">
-                Filmes</Link>
+      <header className="d-flex align-items-center justify-content-between py-3 mb-4 border-bottom">
+  {/* Logo */}
+  <div className="d-flex align-items-center">
+    <Link to="/" className="d-inline-flex align-items-center">
+      <img src={Logo} alt="logo" width={70} height={45} />
+    </Link>
+  </div>
 
-        <Link to="" className="navbar-link">
-          Minha Lista 
-        </Link>
+  {/* Links do menu */}
+  <ul className="nav justify-content-center flex-grow-1">
+    <li>
+      <NavLink to="/" className={({isActive})=> "nav-link px-3 rounded-5" + (isActive ? " bg-white text-dark " : " text-white")}>
+        Filmes
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/minha-lista" className={({isActive})=> "nav-link px-3 rounded-5" + (isActive? " bg-white text-dark " : " text-white")}>
+        Minha Lista
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/categorias" className={({isActive})=> "nav-link px-3 rounded-5" + (isActive? " bg-white text-dark" : " text-white")}>
+        Categorias
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/sobre" className={({isActive})=> "nav-link px-3 rounded-5" + (isActive? " bg-white text-dark " : " text-white")}>
+        Sobre
+      </NavLink>
+    </li>
+  </ul>
 
-        <Link to="" className="navbar-link">
-          Categorias
-        </Link>
+  {/* Botão */}
+  <div className="d-flex">
+    <button type="button" className="btn btn-outline-primary me-2 rounded-5">
+      Login
+    </button>
+  </div>
+</header>
 
-        <Link to="/sobre" className="navbar-link">
-          Sobre
-        </Link>
-      </div>
-    </nav>
+
   );
 };
 
